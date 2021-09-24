5270640a307f61cbf6316f0532418842751ca7dc
noltontarn <noltontarn@gmail.com>
2021-09-24 20:12:10 +0700

    add gas

--- index.js
+++ index.js
@@ -3,33 +3,40 @@ import dotenv from 'dotenv'
 
 dotenv.config()
 
-async function notify() {
-    const token = process.env.LINE_NOTI_TOKEN
-    const form = new URLSearchParams()
-    form.append('message', 'test interval')
-    const response = await fetch('https://notify-api.line.me/api/notify', {
-        method: 'POST', 
-        headers: {
-            'Content-Type': 'application/x-www-form-urlencoded',
-            Authorization: `Bearer ` + token
-        },
-        body: form
-    })
-    console.log({
-        method: 'POST', 
-        headers: {
-            'Content-Type': 'application/x-www-form-urlencoded',
-            Authorization: `Bearer ` + token
-        },
-        body: form
+async function getGasPrice() {
+    const response = await fetch('https://www.gasnow.org/api/v3/gas/price?utm_source=:line-noti-gas', {
+        method: 'GET',
     })
     const data = await response.json()
     console.log('response', data)
+
+    return data
+}
+
+async function notify() {
+    const token = process.env.LINE_NOTI_TOKEN
+    const form = new URLSearchParams()
+    const gas = await getGasPrice()
+    const gasGwei = gas.data.fast/1000000000
+    if (gasGwei <= 43) {
+        form.append('message', `สุลต่านไทม์ Gwei: ${gasGwei}`)
+        const response = await fetch('https://notify-api.line.me/api/notify', {
+            method: 'POST', 
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded',
+                Authorization: `Bearer ` + token
+            },
+            body: form
+        })
+        // const data = await response.json()
+    }
+    // console.log('response', data)
 }
 
 async function run() {
     // await notify()
-    setInterval(notify, 5000)
+    // await getGasPrice()
+    setInterval(notify, 15000)
 }
 
 run()
